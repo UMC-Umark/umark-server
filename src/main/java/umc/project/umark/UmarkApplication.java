3bb0c5e255adf94efcfb82ba30af246684c741ce
구환준/모건 <koojun99@naver.com>
2024-01-13 14:43:42 +0900

    Initial commit

---
+++ src/main/java/umc/project/umark/UmarkApplication.java
@@ -0,0 +1,13 @@
+package umc.project.umark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmarkApplication.class, args);
+    }
+
+}
