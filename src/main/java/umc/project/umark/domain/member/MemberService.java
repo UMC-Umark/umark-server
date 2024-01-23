d2814b98ce330e34c4714f43aec8a70d981aa4c4
SIWON990327 <pos06058@naver.com>
2024-01-23 16:22:33 +0900

    [CHORE]/#16 - MemberService 트랜잭션 처리

--- src/main/java/umc/project/umark/domain/member/MemberService.java
+++ src/main/java/umc/project/umark/domain/member/MemberService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import umc.project.umark.global.exception.GlobalErrorCode;
 import umc.project.umark.global.exception.GlobalException;
 
@@ -63,6 +64,7 @@ public class MemberService {
         return false;
     }
 
+    @Transactional
     public Member signUpMember (String email, String password) throws GlobalException {
 
         Optional <Member> findmember = memberRepository.findByEmail(email);
